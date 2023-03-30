d5b19eeab675e96bdd21409fa426e5e169d66eb3
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2023-03-30 13:45:43 -0400

    update

--- front/components/Add.js
+++ front/components/Add.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import Form from './Form'
 import { sendPost } from '../api-helpers/frontend/utils'
+import { useRouter } from 'next/router'
 
 const Add = () => {
-
+  const router = useRouter();
     const getFormData = (data) => {
-        sendPost(data).then((value) => console.log(value)).catch(err=> console.log(err))
+        sendPost(data).then((value) => console.log(value))
+        router.push("/posts")
+        .catch(err=> console.log(err))
     }
 
   return (
