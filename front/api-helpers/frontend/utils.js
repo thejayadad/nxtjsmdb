34280f06d6694921eb421b4e5d5595c869091780
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2023-03-30 09:05:20 -0400

    update

--- front/api-helpers/frontend/utils.js
+++ front/api-helpers/frontend/utils.js
@@ -2,5 +2,9 @@
 import axios from "axios"
 
 export const getAllPost = async () => {
-    const res = await axios.get("")
+    const res = await axios.get("http://localhost:3000/api/posts")
+    if(res.status !== 200){
+        return new Error("Internal Server Error")
+    }
+    const data = await res.data
 }
